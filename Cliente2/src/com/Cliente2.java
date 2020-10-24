e253346f99b06b0938c0360287ac692d1204ec23
WildIndex <carlossuarez464@gmail.com>
2020-10-24 14:28:22 -0300

    creo que con esto arreglo el setup

--- Cliente2/src/com/Cliente2.java
+++ Cliente2/src/com/Cliente2.java
@@ -3,13 +3,17 @@ package com;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
+import com.services.AreaBeanRemote;
+import com.services.MaterialBeanRemote;
+import com.services.SalonesBeanRemote;
+
 
 
 public class Cliente2 {
 
 	public static void main(String[] args) throws NamingException {
 		
-		AreasBeanRemote areasBean = (AreasBeanRemote) InitialContext.doLookup("Universidad/AreasBean!com.services.AreasBeanRemote");
+		AreaBeanRemote areasBean = (AreaBeanRemote) InitialContext.doLookup("Universidad/AreaBean!com.services.AreaBeanRemote");
 		MaterialBeanRemote materialBean = (MaterialBeanRemote) InitialContext.doLookup("Universidad/MaterialBean!com.services.MaterialBeanRemote");
 		SalonesBeanRemote salonesBean = (SalonesBeanRemote) InitialContext.doLookup("Universidad/SalonesBean!com.services.SalonesBeanRemote");
 		
