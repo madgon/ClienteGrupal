01f86520ea84afa678a8f582cc3d711f09e75928
WildIndex <carlossuarez464@gmail.com>
2020-10-24 13:46:03 -0300

    creados los getters para las clases de Universidad

--- Cliente2/src/com/Cliente2.java
+++ Cliente2/src/com/Cliente2.java
@@ -1,10 +1,17 @@
 package com;
 
+import javax.naming.InitialContext;
 import javax.naming.NamingException;
+
+
 
 public class Cliente2 {
 
 	public static void main(String[] args) throws NamingException {
+		
+		AreasBeanRemote areasBean = (AreasBeanRemote) InitialContext.doLookup("Universidad/AreasBean!com.services.AreasBeanRemote");
+		MaterialBeanRemote materialBean = (MaterialBeanRemote) InitialContext.doLookup("Universidad/MaterialBean!com.services.MaterialBeanRemote");
+		SalonesBeanRemote salonesBean = (SalonesBeanRemote) InitialContext.doLookup("Universidad/SalonesBean!com.services.SalonesBeanRemote");
 		
 
 	}
